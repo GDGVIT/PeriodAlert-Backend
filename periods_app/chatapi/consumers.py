e0407a3c6b24e5eb06faef9962582ddfa50d1780
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-02-10 16:25:22 +0530

    Added comments and established connection between two users by adding them to a layer

--- periods_app/chatapi/consumers.py
+++ periods_app/chatapi/consumers.py
@@ -13,36 +13,107 @@ from fcm_django.models import FCMDevice
 
 
 class ChatConsumer(WebsocketConsumer):
+
+
+    def find_room_name(self, user, receiver, is_request_acceptor):
+        # is_request_acceptor denotes whether the user trying to 
+        # connect to the websocket is the one who wishes to help
+        # is_request_acceptor = 1 -> user who wants to help
+        # is_request_acceptor = 0 -> some other user just trying to connect to channel
+
+        # if user is a request acceptor
+        if is_request_acceptor == '1':
+
+            # Checking if the chat room for the user already exist, 
+            # if yes then connect to the same chat room
+            room = ChatRoom.objects.filter(
+                Q(participant1_id=user.id, participant2_id=receiver.id) |
+                Q(participant1_id=receiver.id, participant2_id=user.id))
+            if len(room) != 0:
+                return room[0].id
+            
+            # Channel or chat room does not exist create a new one
+
+            # Checking requests if it exists or not
+            requset_sent = Requests.objects.filter(user_id=receiver.id)
+            if len(requset_sent) == 0:
+                return None 
+
+            # Creating a new Chat Room
+            room = ChatRoomSerializer(data={
+                "participant1_id":user.id,
+                "participant2_id":receiver.id,
+                "last_message_time":datetime.now()
+            })
+            if room.is_valid():
+                room.save()
+                return room.data['id']
+            else:
+                return None
+
+        # if user is not a request acceptor
+        elif is_request_acceptor == '0':
+            room = ChatRoom.objects.filter(
+                Q(participant1_id=user.id, participant2_id=receiver.id) |
+                Q(participant1_id=receiver.id, participant2_id=user.id))
+
+            if len(room) != 0:
+                return room[0].id
+            else:
+                return None
+
+
     def connect(self):
+        # Getting values from the url
         token = self.scope['url_route']['kwargs']['token']
         is_request_acceptor = self.scope['url_route']['kwargs']['is_request_acceptor']
         receiver_id = self.scope['url_route']['kwargs']['receiver_id']
+
+        # Checking if the user is really valid based on the token 
+        # and if the receiver exists
         try:
             user = Token.objects.get(key=token).user
             receiver = User.objects.get(id=receiver_id)
+            if user == receiver:
+                room_id = None
 
-            self.room_name = "room_id"
-            self.room_group_name = 'chat_room_%s' % self.room_name
-            print(self.room_group_name)
+            # Finding room name for the channel (Based on the chat room id)
+            room_id = self.find_room_name(user, receiver, is_request_acceptor)
 
-            # Join room group
-            async_to_sync(self.channel_layer.group_add)(
-                self.room_group_name,
-                self.channel_name
-            )
+            print(room_id)
 
-            self.accept()
+            if room_id != None:
+
+                self.room_name = room_id
+                self.room_group_name = 'chat_room_%s' % self.room_name
+                print(self.room_group_name)
+
+                # Join room group
+                async_to_sync(self.channel_layer.group_add)(
+                    self.room_group_name,
+                    self.channel_name
+                )               
+
+                self.accept()
+            
+            else:
+
+                self.close()
+
         except Token.DoesNotExist:
             print("Not valid user")
+            self.room_group_name = None
             self.close()  
         
         
     def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
+        if self.room_group_name:
+            async_to_sync(self.channel_layer.group_discard)(
+                self.room_group_name,
+                self.channel_name
+            )
+
 
     # Receive message from WebSocket
     def receive(self, text_data):

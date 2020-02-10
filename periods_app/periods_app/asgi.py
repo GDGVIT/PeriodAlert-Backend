1714eae7faaf682955f467a6139599a0cdd67478
RiddhiGupta5 <mail2riddhigupta@gmail.com>
2020-02-11 02:50:07 +0530

    Changes to asgi file

--- periods_app/periods_app/asgi.py
+++ periods_app/periods_app/asgi.py
@@ -1,13 +1,4 @@
 """
-ASGI config for periods_app project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
-
-"""
 ASGI entrypoint. Configures Django and then runs the application
 defined in the ASGI_APPLICATION setting.
 """
@@ -16,6 +7,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "periods_app.periods_app.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "periods_app.settings")
 django.setup()
 application = get_default_application()
